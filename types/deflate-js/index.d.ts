c12cc027c173184472d47c9aeda766edcb0db7f0
Seb Renauld <seb.renauld@gmail.com>
2020-10-10 12:32:42 +0200

    Deflate - now that our PR to DefinitelyTyped is in

--- types/deflate-js/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'deflate-js' {
-    export function deflate(data:Array<number>, compression?: number):Array<number>;
-    export function inflate(data:Array<number>):Array<number>;
-}
