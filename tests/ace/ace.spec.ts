d92a948f0e3d3a510bbdd0644750ad4e230af9b0
Seb Renauld <seb.renauld@gmail.com>
2020-10-10 18:25:15 +0200

    Fixing WTFs

--- tests/ace/ace.spec.ts
+++ tests/ace/ace.spec.ts
@@ -1,7 +1,16 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import ChaiAsPromised from 'chai-as-promised';
 import * as Ace from '../../src/ace';
 
+chai.use(ChaiAsPromised);
+
+async function garbled() {
+  return Ace.Deserialize('foobar');
+}
 describe('Ace helper method', () => {
+  it('Fails on garbled data', async () => {
+    expect(garbled()).to.be.rejectedWith(Error);
+  });
   it('Understands v1 serialization', async () => {
     const input = '^1^F3^f-2';
     const output = await Ace.Deserialize(input);
